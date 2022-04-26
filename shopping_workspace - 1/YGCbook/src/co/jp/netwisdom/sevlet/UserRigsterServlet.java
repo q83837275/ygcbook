6570f06c616cd0bd406dbae0a623fce213fd6ce0
CAISHUKAI <q993068966@gmail.com>
2022-04-26 16:11:14 +0900

    1-2

--- shopping_workspace - 1/YGCbook/src/co/jp/netwisdom/sevlet/UserRigsterServlet.java
+++ shopping_workspace - 1/YGCbook/src/co/jp/netwisdom/sevlet/UserRigsterServlet.java
@@ -7,28 +7,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import cn.key.dao.BookDiscussDAO;
+import co.jp.netwisdom.dao.UserinfoDAO;
+import co.jp.netwisdom.entity.UserInfo;
 
 public class UserRigsterServlet extends HttpServlet {
 
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		int id = Integer.parseInt(request.getParameter("disId"));
-		BookDiscussDAO dao = new BookDiscussDAO();
-		String path = "/Error.jsp";
-		String returnPath = "/ShowDiscussServlet";
-		if (dao.delete(id)) {
-			path = "/Success.jsp";
-		}
-		request.setAttribute("title","����ɾ��");
-		request.setAttribute("returnPath",returnPath);
-		request.getRequestDispatcher("/background"+ path).forward(request, response);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String sex = request.getParameter("sex");
+		String major = request.getParameter("major");
+		String intro = request.getParameter("intro");
+		//TO DO
+		String hobby = request.getParameter("hobby");
+		
+		UserinfoDAO dao = new  UserinfoDAO();
+		//用户信息表更新
+		if(dao.save(new UserInfo(username, password, sex, major, intro))){
+			System.out.println("用户信息表更新成功");
+		}else{
+			System.out.println();
+		};
+		//TO DO 用户爱好表
+
 	}
 
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		this.doGet(request, response);
+
 	}
 
 }
